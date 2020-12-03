d62795cc78c5221a31f4f8822cb9ded20d0bd5b7
arthurhero <chenziwe@grinnell.edu>
2020-12-03 15:11:51 +0800

    too save time

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ if __name__ == '__main__':
     # experiment code 
 
     '''
-    percents = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9] # percentage of missing pixels
-    num_holes = [2,5,10,50,100,500,1000,5000,10000] # number of square holes
-    num_img = 30
+    percents = [0.1,0.2,0.5,0.9] # percentage of missing pixels
+    num_holes = [2,5,10,100,1000,10000] # number of square holes
+    num_img = 10
     '''
     percents = [0.9] # percentage of missing pixels
     num_holes = [1000] # number of square holes
-    num_img = 1
+    num_img = 2
 
     for p in range(len(percents)):
         for nh in range(len(num_holes)):
@@ -24,14 +24,17 @@ if __name__ == '__main__':
             mc_l1 = 0.0
             fo_l1 = 0.0 # l1 difference between restored and orig img
             for i in range(len(imgs)):
+                print("img:",i)
                 img = imgs[i]
                 #display_img(img)
                 mask = masks[i]
                 #display_img(apply_mask(img,mask))
-                fo_restored_img = fourier_compressive_sensing(img,mask) # get restored img using compressive sensing
-                #display_img(fo_restored_img)
                 mc_restored_img = matrix_completion(img,mask) # get restored img using matrix completion
+                print("mc:",np.sum(np.abs(mc_restored_img-img))/(np.sum(mask)))
                 #display_img(mc_restored_img)
+                fo_restored_img = fourier_compressive_sensing(img,mask) # get restored img using compressive sensing
+                print("fo:",np.sum(np.abs(fo_restored_img-img))/(np.sum(mask)))
+                #display_img(fo_restored_img)
                 mc_l1 += np.sum(np.abs(mc_restored_img-img))/(np.sum(mask)) # take average l1 diff
                 fo_l1 += np.sum(np.abs(fo_restored_img-img))/(np.sum(mask)) # take average l1 diff
             mc_l1 /= len(imgs)
