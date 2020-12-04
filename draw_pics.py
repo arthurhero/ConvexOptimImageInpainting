a0d2a89ef9e3b9fb6543055af876ae58f836b74c
arthurhero <chenziwe@grinnell.edu>
2020-12-04 10:05:06 +0800

    draw pics

--- draw_pics.py
+++ draw_pics.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 
-def draw_line_plot(x,y,title,filename,xlabel,ylabel,z=None,xleg=None,zleg=None):
+def draw_line_plot(x,y,title,filename,xlabel,ylabel,z=None,yleg=None,zleg=None):
     '''
-    draw line plot x,z vs y
+    draw line plot y,z vs x
     x - x data points
     y - y data points
     title - plot title
     filename - save to filename
     z - optinal another line
-    xleg - x legend
+    yleg - y legend
     '''
     fig = plt.figure()
     fig.suptitle(title)
@@ -20,7 +20,7 @@ def draw_line_plot(x,y,title,filename,xlabel,ylabel,z=None,xleg=None,zleg=None):
     if z is None:
         plot1.plot(x, y)
     else:
-        plot1.plot(x,y,'r.-',label=xleg)
+        plot1.plot(x,y,'r.-',label=yleg)
         plot1.plot(x,z,'b.-',label=zleg)
         plot1.legend()
     plt.savefig(filename)
@@ -28,6 +28,37 @@ def draw_line_plot(x,y,title,filename,xlabel,ylabel,z=None,xleg=None,zleg=None):
 
 def read_file_and_plot(filename):
     lines = [line.rstrip('\n') for line in open(filename)]
+    mc_l1_dict = dict() # key: (percent, num_hole), value: l1 diff
+    fo_l1_dict = dict()
+    percents = set()
+    num_holes = set()
+
+    percent=""
+    num_hole=""
+
+    for i in range(len(lines)):
+        line = lines[i]
+        if "pencent" in line:
+            percent = line.split(':')[1].strip()
+            percents.add(percent)
+        if "num_hole" in line:
+            num_hole = line.split(':')[1].strip()
+            num_holes.add(num_hole)
+        if "mc_l1" in line:
+            mc_l1 = float(line.split(':')[1])
+            mc_l1_dict[(percent,num_hole)] = mc_l1
+        if "fo_l1" in line:
+            fo_l1 = float(line.split(':')[1])
+            fo_l1_dict[(percent,num_hole)] = fo_l1
+    for p in percents:
+        xs = list()
+        ys = list()
+        zs = list()
+        for nh in num_holes:
+            xs.append(nh)
+            ys.append(mc_l1_dict[(p,nh)])
+            zs.append(fo_l1_dict[(p,nh)])
+        draw_line_plot(xs,ys,"l1 Diff vs number of holes under "+p+" pixel loss","p"+p+".jpg",'Number of Square Holes',"L1 Diff (per pixel avg)",zs,'Matrix Completion','Cosine Compressive Sensing')
 
 
 if __name__ == '__main__':
